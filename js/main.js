82ff6147bafd0e61de4fe122fc32d6fbe4ab7a52
fariha <fariha.fariha.chy@gmail.com>
2020-08-03 22:40:29 +0600

    portfolio
    happy

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// navbar starts
+const slide = ()=>{
+    const hamburger = document.querySelector(".hamburger");
+    const items = document.querySelector(".nav-items");
+
+    hamburger.addEventListener('click', ()=>{
+        items.classList.toggle('nav-active');
+        hamburger.classList.toggle('change');
+    });
+
+
+
+}
+slide();
+
+AOS.init({
+    easing:'ease',
+    duration:1500
+});
